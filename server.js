2be4cf005b784fc77755cbe4e4648d7415f79ce5
Michael Cassidy <mikecassidy0000@gmail.com>
2019-01-26 18:20:54 -0500

    keys

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/key").mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
