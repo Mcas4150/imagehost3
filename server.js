e127af2da630ad18be91f77c5eb4393dfb6399b9
Michael Cassidy <mikecassidy0000@gmail.com>
2019-01-26 19:17:38 -0500

    cors

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
